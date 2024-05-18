41182b62aa47adc5c75bab8a3f6688318570ff46
Jupiter Group <ourjupitergroup@gmail.com>
2024-05-18 17:24:41 -0400

    2

--- products/OmegaTerminal/files/config.js
+++ products/OmegaTerminal/files/config.js
@@ -1,3 +1,4 @@
 const config = {
-    weatherApiKey: 'cee5a38a036461af9b79be361322bfd2'
+    weatherApiKey: 'cee5a38a036461af9b79be361322bfd2',
+    geminiApiKey: 'AIzaSyAZYogc8ENVCoWB10cVYJOv0gPUI5cGqSA'
 };
